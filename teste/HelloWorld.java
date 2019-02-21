b3cdb9cc1dbc34460698afdd34eba8430a885de1
Gustavo Dias <gustavodias2@gmail.com>
2019-02-21 14:05:46 -0300

    alterando a mesma linha que outra pessoa

--- teste/HelloWorld.java
+++ teste/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     
-    private int Numero = 0;
+    private static int numeroInteiro = 100;
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");

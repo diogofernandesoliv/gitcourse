319382f6b715f6eb2acc04fe470a52ef27e9d9b5
Gustavo Dias <gustavodias2@gmail.com>
2019-02-21 11:57:54 -0300

    voltando para o iniciio da frente

--- teste/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
-}
-// Arquivo editado por GPD38
